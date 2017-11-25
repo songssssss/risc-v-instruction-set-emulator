5ceb70e5efbb2ab322e0d41dcc93006b5c557101
Sayan Ray Chaudhuri <sayanrayc@gmail.com>
2017-11-25 00:53:46 -0800

    commit

--- part1.c
+++ part1.c
@@ -20,8 +20,40 @@ void write_ecall(Instruction);
 
 void decode_instruction(Instruction instruction) {
     /* YOUR CODE HERE: COMPLETE THE SWITCH STATEMENTS */
-    switch(0) { // What do we switch on?
+    switch(instruction.opcode) { // What do we switch on?
         /* YOUR CODE HERE */
+        case 0x33:
+            write_rtype(instruction);
+            break;
+
+        case 0x03:
+            write_load(instruction);
+            break;
+
+        case 0x13:
+            write_itype_except_load(instruction);
+            break;
+
+        case 0x73:
+            write_ecall(instruction);
+            break;
+
+        case 0x23:
+            write_store(instruction);
+            break;
+
+        case 0x63:
+            write_branch(instruction);
+            break;
+
+        case 0x37:
+            write_lui(instruction);
+            break;
+
+        case 0x6f:
+            write_jal(instruction);
+            break;
+
     	default: // undefined opcode
             handle_invalid_instruction(instruction);
     	    break;
@@ -29,28 +61,125 @@ void decode_instruction(Instruction instruction) {
 }
 
 void write_rtype(Instruction instruction) {
-	switch(0) { // What do we switch on?
+    char *name;
+	switch(instruction.rtype.funct3) { // What do we switch on?
         /* YOUR CODE HERE */
+        case 0x0:
+            if (instruction.rtype.funct7 == 0x00) {
+                name = "add";
+            } else if (instruction.rtype.funct7 == 0x01) {
+                name = "mul";
+            } else {
+                name = "sub";
+            }
+            break;
+
+        case 0x1:
+            if (instruction.rtype.funct7 == 0x00) {
+                name = "sll";
+            } else {
+                name = "mulh";
+            }
+            break;
+
+        case 0x2:
+            name = "slt";
+            break;
+
+        case 0x4:
+            if (instruction.rtype.funct7 == 0x00) {
+                name = "xor";
+            } else {
+                name = "div";
+            }
+            break;
+
+        case 0x5:
+            if (instruction.rtype.funct7 == 0x00) {
+                name = "srl";
+            } else {
+                name = "sra";
+            }
+            break;
+
+        case 0x6:
+            if (instruction.rtype.funct7 == 0x00) {
+                name = "or";
+            } else {
+                name = "rem";
+            }
+            break;
+
+        case 0x7:
+            name = "and";
+            break;
+
 	    default:
             handle_invalid_instruction(instruction);
             break;
 	}
+    print_rtype(name, instruction);
 }
 
 void write_itype_except_load(Instruction instruction) {
     int shiftOp;
     shiftOp = -1;
-    switch(0) { // What do we switch on?
+    switch(instruction.itype.funct3) { // What do we switch on?
         /* YOUR CODE HERE */
+        case 0x0:
+            print_itype_except_load("addi", instruction, bitSigner(instruction.itype.imm, 12));
+            break;
+
+        case 0x1:
+            print_itype_except_load("slli", instruction, bitSigner(instruction.itype.imm, 12));
+            break;
+
+        case 0x2:
+            print_itype_except_load("slti", instruction, bitSigner(instruction.itype.imm, 12));
+            break;
+
+        case 0x4:
+            print_itype_except_load("xori", instruction, bitSigner(instruction.itype.imm, 12));
+            break;
+
+        case 0x5:
+            shiftOp = (instruction.itype.imm & 0x01f);
+            if ((instruction.itype.imm & 0xfe0) == 0x00) {
+                print_itype_except_load("srli", instruction, shiftOp);
+            } else {
+                print_itype_except_load("srai", instruction, shiftOp);
+            }
+            break;
+
+        case 0x6:
+            print_itype_except_load("ori", instruction, bitSigner(instruction.itype.imm, 12));
+            break;
+
+        case 0x7:
+            print_itype_except_load("andi", instruction, bitSigner(instruction.itype.imm, 12));
+            break;
+
         default:
             handle_invalid_instruction(instruction);
-            break;  
+            break;
     }
 }
 
 void write_load(Instruction instruction) {
-    switch(0) { // What do we switch on?
+    switch(instruction.itype.funct3) { // What do we switch on?
         /* YOUR CODE HERE */
+        case 0x0:
+            print_load("lb", instruction);
+            break;
+
+        case 0x1:
+            print_load("lh", instruction);
+            break;
+
+        case 0x2:
+            print_load("lw", instruction);
+            break;
+
         default:
             handle_invalid_instruction(instruction);
             break;
@@ -58,8 +187,20 @@ void write_load(Instruction instruction) {
 }
 
 void write_store(Instruction instruction) {
-    switch(0) { // What do we switch on?
+    switch(instruction.itype.funct3) { // What do we switch on?
         /* YOUR CODE HERE */
+        case 0x0:
+            print_store("sb", instruction);
+            break;
+
+        case 0x1:
+            print_store("sh", instruction);
+            break;
+
+        case 0x2:
+            print_store("sw", instruction);
+            break;
+
         default:
             handle_invalid_instruction(instruction);
             break;
@@ -67,8 +208,16 @@ void write_store(Instruction instruction) {
 }
 
 void write_branch(Instruction instruction) {
-    switch(0) { // What do we switch on?
+    switch(instruction.itype.funct3) { // What do we switch on?
         /* YOUR CODE HERE */
+        case 0x0:
+            print_branch("beq", instruction);
+            break;
+
+        case 0x1:
+            print_branch("bne", instruction);
+            break;
+
         default:
             handle_invalid_instruction(instruction);
             break;
@@ -79,32 +228,43 @@ void write_branch(Instruction instruction) {
 
 void write_lui(Instruction instruction) {
     /* YOUR CODE HERE */
+    printf(LUI_FORMAT, instruction.utype.rd, (sWord) instruction.utype.imm);
 }
 
 void write_jal(Instruction instruction) {
     /* YOUR CODE HERE */
+    printf(JAL_FORMAT, instruction.ujtype.rd, get_jump_offset(instruction));
 }
 
 void write_ecall(Instruction instruction) {
     /* YOUR CODE HERE */
+    printf(ECALL_FORMAT);
 }
 
 void print_rtype(char *name, Instruction instruction) {
     /* YOUR CODE HERE */
+    printf(RTYPE_FORMAT, name, instruction.rtype.rd, instruction.rtype.rs1, instruction.rtype.rs2);
 }
 
 void print_itype_except_load(char *name, Instruction instruction, int imm) {
     /* YOUR CODE HERE */
+    printf(ITYPE_FORMAT, name, instruction.itype.rd, instruction.itype.rs1, (sWord) imm);
 }
 
 void print_load(char *name, Instruction instruction) {
     /* YOUR CODE HERE */
+    printf(MEM_FORMAT, name, instruction.itype.rd, (sWord) bitSigner(instruction.itype.imm, 12),
+        instruction.itype.rs1);
 }
 
 void print_store(char *name, Instruction instruction) {
     /* YOUR CODE HERE */
+    printf(MEM_FORMAT, name, instruction.stype.rs2, get_store_offset(instruction),
+        instruction.stype.rs1);
 }
 
 void print_branch(char *name, Instruction instruction) {
     /* YOUR CODE HERE */
+    printf(BRANCH_FORMAT, name, instruction.sbtype.rs1, instruction.sbtype.rs2,
+        get_branch_offset(instruction));
 }
--- utils.c
+++ utils.c
@@ -4,26 +4,40 @@
 
 //sign extends a bitfield with given size
 /* You may find implementing this function helpful */
-int bitSigner( unsigned int field, unsigned int size){
+int bitSigner(unsigned int field, unsigned int size) {
     /* YOUR CODE HERE */
-    return 0;
+    int value = field;
+    value = ((value << (32 - size)) >> (32 - size));
+    return value;
 }
 
 /* Remember that the offsets should return the offset in BYTES */
-
 int get_branch_offset(Instruction instruction) {
     /* YOUR CODE HERE */
-    return 0;
+    int offset = 0x00000000;
+    offset = offset | (0x0000001e & instruction.sbtype.imm5);
+    offset = offset | (0x000007e0 & (instruction.sbtype.imm7 << 5));
+    offset = offset | (0x00000800 & (instruction.sbtype.imm5 << 11));
+    offset = offset | (0x00001000 & (instruction.sbtype.imm7 << 6));
+    return bitSigner(offset, 13);
 }
 
 int get_jump_offset(Instruction instruction) {
     /* YOUR CODE HERE */
-    return 0;
+    int offset = 0x00000000;
+    offset = offset | (0x000ff000 & (instruction.ujtype.imm << 12));
+    offset = offset | (0x00000800 & (instruction.ujtype.imm << 3));
+    offset = offset | (0x000007fe & (instruction.ujtype.imm >> 8));
+    offset = offset | (0x00100000 & (instruction.ujtype.imm << 2));
+    return bitSigner(offset, 21);
 }
 
 int get_store_offset(Instruction instruction) {
     /* YOUR CODE HERE */
-    return 0;
+    int offset = 0x00000000;
+    offset = offset | (0x00000001f & instruction.stype.imm5);
+    offset = offset | (0x000000fe0 & (instruction.stype.imm7 << 5));
+    return bitSigner(offset, 12);
 }
 
 void handle_invalid_instruction(Instruction instruction) {
@@ -39,4 +53,3 @@ void handle_invalid_write(Address address) {
     printf("Bad Write. Address: 0x%08x\n", address);
     exit(-1);
 }
-
